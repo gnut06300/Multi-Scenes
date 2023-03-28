48b500d5e66bc6d9dc312f0c07cf4c06103aba02
Gerald Col <gnut@gnut.eu>
2023-03-28 16:57:17 +0200

    Setup VRTK 11mn55

---
+++ Assets/WebTrigger.cs
@@ -0,0 +1,14 @@
+using WebXR;
+using Zinnia.Action;
+
+public class WebTrigger : BooleanAction
+{
+
+    public WebXRController controller;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Receive( controller.GetButton(WebXRController.ButtonTypes.Trigger) );
+    }
+}
